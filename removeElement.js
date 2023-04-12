22ba2ada62d78ce26bb68ac93aaca211b42a8ff4
unknown <a01709522@tec.mx>
2023-04-11 23:21:35 -0600

    solve removeElement

---
+++ removeElement.js
@@ -0,0 +1,20 @@
+
+// @param {number[]} nums
+// @param {number} val
+// @return {number}
+
+
+const nums = [3, 2, 2, 3];
+const val = 3;
+
+var removeElement = function(nums, val) {
+    let k = 0;
+    for (let i = 0; i < nums.length; i++){
+        if (nums[i] !== val){
+            nums[k++] = nums[i];
+        }
+    }
+    return k, nums;
+};
+
+const k = removeElement(nums, val);
