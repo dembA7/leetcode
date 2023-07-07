724f577668759d83df412ab8f1fc43b33a9cf2a2
unknown <a01709522@tec.mx>
2023-07-07 04:26:28 -0600

    solve climbingStairs

---
+++ climbingStairs.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    if (n <= 2) {
+        return n;
+    }
+    
+    let prev1 = 1;
+    let prev2 = 2;
+    
+    for (let i = 3; i <= n; i++) {
+        const curr = prev1 + prev2;
+        prev1 = prev2;
+        prev2 = curr;
+    }
+    
+    return prev2;
+};
+
+let k = 7;
+console.log(climbStairs(k))
