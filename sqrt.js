f1c75cad25152b8a980cd7a115e83516c7eb280a
unknown <a01709522@tec.mx>
2023-07-08 02:37:34 -0600

    solve sqrt

--- sqrt.js
+++ sqrt.js
@@ -3,25 +3,25 @@
  * @return {number}
  */
 var mySqrt = function(x) {
-    if (x === 0) {
-        return 0;
-      }
-    
-      let left = 1;
-      let right = Math.floor(x / 2);
-    
-      while (left <= right) {
-        const mid = Math.floor((left + right) / 2);
-        const square = mid * mid;
-    
-        if (square === x) {
-          return mid;
-        } else if (square > x) {
-          right = mid - 1;
-        } else {
-          left = mid + 1;
-        }
-      }
-    
-      return right;
+  if (x === 0 || x === 1) {
+    return x;
+  }
+
+  let left = 1;
+  let right = x;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    const square = mid * mid;
+
+    if (square === x) {
+      return mid;
+    } else if (square < x) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return right;
 };
