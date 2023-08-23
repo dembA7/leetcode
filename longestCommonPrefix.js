a39ee65fdfbd88a37cde0b17a64e835bf537c360
unknown <a01709522@tec.mx>
2023-08-22 21:34:16 -0600

    add definitions

--- longestCommonPrefix.js
+++ longestCommonPrefix.js
@@ -2,20 +2,22 @@
  * @param {string[]} strs
  * @return {string}
  */
-
 var longestCommonPrefix = function(strs) {
-
-    if (strs.length === 0) return "";
-
     let prefix = strs[0];
-    for (let i = 0; i < strs.length; i++){
-        while (strs[i].indexOf(prefix) !== 0){
-            prefix = prefix.slice(0, prefix.length - 1);
-            if (prefix.length === 0) return "";
+
+    for (let i = 1; i < strs.length; i++){
+        while (strs[i].indexOf(prefix) != 0){
+            prefix = prefix.substring(0, prefix.length - 1)
         }
     }
     return prefix;
-}
+};
 
-const strs = [];
-console.log(longestCommonPrefix(strs));
+let array = ["flower","flow","flight"];
+
+// indexOf compara dos cadenas y retorna 0 si los caracteres de la primera cadena 
+// coinciden con los de la segunda (de izquierda a derecha), para todos los demás valores retorna -1
+console.log(array[0].indexOf("flower"));
+
+// Substring elimina los caracteres de un string dado el rango
+console.log(array[0].substring(0, array[0].length -1));
